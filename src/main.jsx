c611548d2a98e8dbc464d48e650a27a1efac6eea
jiyanru125-web <jiyanru125@gmail.com>
2026-05-07 18:01:26 +0800

    Add React app entry point

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import IELTSBattleRoom from './App.jsx';
+
+createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <IELTSBattleRoom />
+  </React.StrictMode>
+);
