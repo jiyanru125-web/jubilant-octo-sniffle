0758fecc6cc85e5e7d25f862937660c802ec38bd
jiyanru125-web <jiyanru125@gmail.com>
2026-05-09 00:12:42 +0800

    Use daily persistent task app

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import IELTSBattleRoom from './AppSupabaseFixed.jsx';
+import IELTSBattleRoom from './AppDaily.jsx';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
