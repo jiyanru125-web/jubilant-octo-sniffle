d51e95461f34d10e97274b1969758da620366d22
jiyanru125-web <jiyanru125@gmail.com>
2026-05-09 00:27:37 +0800

    Use flame and compact history app

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import IELTSBattleRoom from './AppDaily.jsx';
+import IELTSBattleRoom from './AppDailyV2.jsx';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
