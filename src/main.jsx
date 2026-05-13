e84f8386ad8ff03f1e5baf92240e20b890380db1
jiyanru125-web <jiyanru125@gmail.com>
2026-05-14 00:32:07 +0800

    Use fast upload app

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import IELTSBattleRoom from './AppDailyV2.jsx';
+import IELTSBattleRoom from './AppDailyFastUpload.jsx';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
